fa76a170a95d749fd5de4a68a24371abe2ebcc6d
Angelina M <angelina.moshnenko@mail.ru>
2022-10-07 17:59:32 +0300

    Finished 1 lab

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :jobs
       resources :companies do
+
         resources :jobs
       end
+
+      match '/:id/mark_deleted', to: 'companies#mark_deleted', via: :put
+
       resources :applies
+
       resources :geeks
-      resources :companies
+
+
+
     end
   end
   match "*path", to: "application#catch_404", via: :all
