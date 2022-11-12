a95d5e44f49cfc9b991e04ca464d7e0d174f157a
Angelina M <angelina.moshnenko@mail.ru>
2022-11-12 21:18:52 +0300

    GraphQL

--- Gemfile
+++ Gemfile
@@ -11,8 +11,8 @@ gem "puma", "~> 5.0"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 gem "sqlite3", "~> 1.4"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-
-
+gem "graphql"
+gem 'graphiql-rails'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   scope module: 'api' do
     namespace :v1 do
       resources :jobs
@@ -12,10 +13,24 @@ Rails.application.routes.draw do
       resources :applies
 
       resources :geeks
+    end
+
+    namespace :v2 do
+      get 'company/index'
+      get 'company/show'
+      get 'job/show'
+      get 'job/index'
 
 
 
     end
+  end
+
+  post '/graphql', to: 'graphql#execute'   # Graphql POST запросы-
+  get '/graphql', to: 'graphql#execute'
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   match "*path", to: "application#catch_404", via: :all
 
